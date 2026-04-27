91a1f48d02eef372f78a95eebb3fb511214f9aad
Pablo <71288368+PapayaSupreme@users.noreply.github.com>
2026-04-27 11:44:46 +0200

    main menu pushed

---
+++ src/main.py
@@ -0,0 +1,59 @@
+from src.utilities import vectorize_images
+
+import numpy as np
+
+if __name__ == "__main__":
+    print("Loading data...")
+    X, y = vectorize_images.load_vectorized_digits(
+        normalize=True,
+        save_vectors=True,
+        save_debug=True,
+    )
+
+    print(f"X shape: {X.shape}, dtype: {X.dtype}")
+    print(f"y shape: {y.shape}, labels: {np.unique(y)}")
+    print("Saved vectors to: data/vectorized_output/X_vectors.npy")
+    print("Saved labels to:  data/vectorized_output/y_labels.npy")
+    print("Saved debug files to: data/vectorized_output/debug/")
+
+    print("=== DATA IS READY ===")
+    end = False
+    while not end:
+        choice = -1
+        while choice < 0 or choice > 6:
+            print("\n=== MAIN MENU ===")
+            print("1. (re)Load data")
+            print("2. [WIP] Train & test the linear Model")
+            print("3. [WIP] Train & test the cross entropy / log loss model")
+            print("4. [WIP] Train & test the gradient descent model")
+            print("5. [WIP] Error calculation")
+            print("6. [WIP] Train & test the neural network model")
+            print("0. EXIT")
+            choice = int(input("Enter your choice: "))
+        if choice == 1:
+            print("Loading data...")
+            X, y = vectorize_images.load_vectorized_digits(
+                normalize=True,
+                save_vectors=True,
+                save_debug=True,
+            )
+
+            print(f"X shape: {X.shape}, dtype: {X.dtype}")
+            print(f"y shape: {y.shape}, labels: {np.unique(y)}")
+            print("Saved vectors to: data/vectorized_output/X_vectors.npy")
+            print("Saved labels to:  data/vectorized_output/y_labels.npy")
+            print("Saved debug files to: data/vectorized_output/debug/")
+        elif choice == 2:
+            print("[WIP]")
+        elif choice == 3:
+            print("[WIP]")
+        elif choice == 4:
+            print("[WIP]")
+        elif choice == 5:
+            print("[WIP]")
+        elif choice == 6:
+            print("[WIP]")
+        elif choice == 0:
+            end = True
+    print("Shutting down...")
+
