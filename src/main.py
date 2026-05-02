74d52005e81501980ef566db7c12b96af879dff5
KurumiiiiDaKat <67027032+KurumiiiiDaKat@users.noreply.github.com>
2026-05-02 15:52:35 +0200

    neural networks

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 from src.models.linear import LinearMultiClassModel
+from src.models.neural_network import NeuralNetworkModel
 from src.utilities.vectorize_images import load_vectorized_digits
 
 import numpy as np
-
+import torch
 
 
 if __name__ == "__main__":
@@ -62,7 +63,32 @@ if __name__ == "__main__":
         elif choice == 5:
             print("[WIP]")
         elif choice == 6:
-            print("[WIP]")
+            for a in range(2):
+                models, losses = [], []
+                for j in range(30):
+                    nn_model = NeuralNetworkModel(784, 10, a + 1, 16)
+
+                    loss = 0
+                    for i in range(len(X)):
+                        image, result = X[i], y[i]
+                        prediction = nn_model.forward(image)
+                        loss += -np.log(prediction[result] + 0.00001)
+                        nn_model.its_backpropagation_time(prediction, image, result, 0.05)
+                    avg_loss = loss / len(X)
+
+                    models.append(nn_model)
+                    losses.append(avg_loss)
+                    print(str(j) + " : " + str(round(avg_loss, 4)))
+
+                minIndex = 0
+                for k in range(len(losses)):
+                    if losses[k] < losses[minIndex]: minIndex = k
+                print("Minimum on model " + str(minIndex) + " at loss = " + str(round(losses[minIndex], 4)))
+                torch.save(models[minIndex].state_dict(), str(a + 1) + "_hidden.pth")
+
+        elif choice == 7:
+            nn_model = NeuralNetworkModel(784, 10, 1, 3)
+            nn_model.load_state_dict(torch.load("1_hidden.pth"))
         elif choice == 0:
             end = True
     print("Shutting down...")
